c5eccecc800097c250d35bcd251e0c22ac5561d2
Mark <17260591+MarkKleinhaus@users.noreply.github.com>
2024-11-04 20:46:57 -0800

    get some debug in

--- blog/settings.py
+++ blog/settings.py
@@ -34,7 +34,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
