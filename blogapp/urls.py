88d893f01c336e62a62a51dded848e5c4b81b29d
Mark <17260591+MarkKleinhaus@users.noreply.github.com>
2024-11-05 12:03:43 -0800

    add a couple pages

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('current_projects/', views.current_projects, name='current_projects'),
+    path('past_projects/', views.past_projects, name='past_projects')
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def current_projects(request):
+    return render(request, 'current_projects.html')
+
+
+def past_projects(request):
+    return render(request, 'past_projects.html')
