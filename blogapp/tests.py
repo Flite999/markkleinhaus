a555d690d1687f56b5f4aab6c3552772ab8e4afa
Mark <17260591+MarkKleinhaus@users.noreply.github.com>
2024-11-05 14:33:40 -0800

    add some tests

--- blogapp/tests.py
+++ blogapp/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+
+class NavigationTests(TestCase):
+    def test_current_projects_page_status_code(self):
+        response = self.client.get(reverse('current_projects'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_past_projects_page_status_code(self):
+        response = self.client.get(reverse('past_projects'))
+        self.assertEqual(response.status_code, 200)
